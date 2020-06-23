c3f9dd20963ec1b5e288eafa5f5b0eaf0ceeaa94
rex662624 <zfk.662624@gmail.com>
2020-06-23 17:01:16 +0800

    Stop1 outer defect complete
    TODO:
    1. MSER algorithm

--- Online Program/Stop1.cs
+++ Online Program/Stop1.cs
@@ -12,9 +12,10 @@ namespace TestStop1
         static void Main(string[] args)
         {
             //量時間
-            
+
             //讀圖
             string[] filenamelist = Directory.GetFiles(@".\images\", "*.jpg", SearchOption.AllDirectories);
+            //string[] filenamelist = Directory.GetFiles(@".\images\", "117.jpg", SearchOption.AllDirectories);
             //debug
             int fileindex = 0;
 
@@ -22,28 +23,45 @@ namespace TestStop1
             {
                 fileindex++;
                 Mat src = Cv2.ImRead(filename, ImreadModes.Grayscale);
-                Mat vis_rgb = src.CvtColor(ColorConversionCodes.GRAY2RGB);
-                //Console.WriteLine(vis_rgb.Size()+"  "+vis_rgb.Channels());
+                //Console.WriteLine(filename);
+                Stop1_Detect(src, fileindex);
 
-                var watch = new System.Diagnostics.Stopwatch();
-                watch.Start();
-                //==========================algorithm===============================
-
-                //mask the iner part noise of src
-                List<Point[]> contours_final = Mask_innercicle(ref src);
-
-                src.SaveImage("./result/test" + fileindex + ".jpg");
-                //vis_rgb.SaveImage("./result/test"+ fileindex + ".jpg");
-                //==================================================================
-                watch.Stop();
-
-                //印出時間
-                Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
             }
-        
+
             Console.ReadLine();
         }
+        static void Stop1_Detect(Mat src, int fileindex)
+        {
 
+            Mat vis_rgb = src.CvtColor(ColorConversionCodes.GRAY2RGB);
+            //Console.WriteLine(vis_rgb.Size()+"  "+vis_rgb.Channels());
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            //==========================algorithm===============================
+
+            //mask the iner part noise of src
+            List<Point[]> contours_final = Mask_innercicle(ref src);
+            int nLabels = 0;//number of labels
+            var stats = FindContour_and_outer_defect(src, contours_final,ref nLabels, fileindex);
+            for (int i = 0; i < nLabels; i++)
+            {
+                int area = stats[i, 4];
+                if (area < 200000)
+                {
+                    vis_rgb.Rectangle(new Rect(stats[i,0], stats[i, 1],stats[i, 2],stats[i, 3]), Scalar.Green,3);
+                }
+            }
+
+            //src.SaveImage("./result/test" + fileindex + ".jpg");
+            vis_rgb.SaveImage("./result/test"+ fileindex + ".jpg");
+            //==================================================================
+            watch.Stop();
+
+            //印出時間
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        }
         //mask the inner part of circle 
         static List<Point[]> Mask_innercicle(ref Mat img)
         {
@@ -84,17 +102,47 @@ namespace TestStop1
 
             return contours_final;
         }
-        /*
-        static void FindContour_and_outer_defect(Mat img, List<Point[]> contours_final)
+        
+        //Find outer defect
+        static int[,] FindContour_and_outer_defect(Mat img, List<Point[]> contours_final,ref int nLabels, int fileindex)
         {
-            Point[] Convex_hull = Cv2.ConvexHull(contours_final[0]);
-
+            // variable
             OpenCvSharp.Point[][] temp = new Point[1][];
-            temp[0] = contour_now;
+
+            
+            // Convex hull
+            Point[] Convex_hull = Cv2.ConvexHull(contours_final[0]);
+            temp[0] = Convex_hull;
             Mat convex_mask_img = Mat.Zeros(img.Size(), MatType.CV_8UC1);
-            Cv2.DrawContours(convex_mask_img, Convex_hull, -1, 0, -1)
+            Cv2.DrawContours(convex_mask_img, temp, -1, 255, -1);
+            
+
+            // Contour
+            temp[0] = contours_final[0];
+            Mat contour_mask_img = Mat.Zeros(img.Size(), MatType.CV_8UC1);
+            Cv2.DrawContours(contour_mask_img, temp, -1, 255, -1);
+
+            // Subtraction 
+            Mat diff_image = convex_mask_img - contour_mask_img;
+            
+
+            //Opening
+            Mat kernel = Mat.Ones(2, 2, MatType.CV_8UC1);//改變凹角大小
+            diff_image = diff_image.MorphologyEx(MorphTypes.Open, kernel);
+
+            //diff_image.SaveImage("./result/test" + fileindex + ".jpg");
+            //Connected Component
+            var labelMat = new MatOfInt();
+            var statsMat = new MatOfInt();// Row: number of labels Column: 5
+            var centroidsMat = new MatOfDouble();
+            nLabels = Cv2.ConnectedComponentsWithStats(diff_image, labelMat, statsMat, centroidsMat);
+
+            var labels = labelMat.ToRectangularArray();
+            var stats = statsMat.ToRectangularArray();
+            var centroids = centroidsMat.ToRectangularArray();
+
+            return stats;
 
         }
-        */
     }
 }
