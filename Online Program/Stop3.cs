2c7119f5e99cfd07f34e51cf5b37920f68089d94
rex662624 <zfk.662624@gmail.com>
2020-07-12 14:40:10 +0800

    Stop3 linspace + Gaussian 1D complete
    I use LineIterator to achieve the same result by the linspace and
    Gaussian1D.
    
    Also, I write a python program to plot the data.

--- Online Program/Stop3.cs
+++ Online Program/Stop3.cs
@@ -5,6 +5,7 @@ using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using System.Threading;
 using System.Linq;
+using NumSharp;
 
 namespace Stop3
 {
@@ -83,11 +84,11 @@ namespace Stop3
             Cv2.MinEnclosingCircle(contours_final[0], out center, out radius);
 
             Mat mask_img = Mat.Zeros(Src.Size(), MatType.CV_8UC1);
-            Cv2.Circle(mask_img, (OpenCvSharp.Point)center, (int)(radius +7),255, thickness: -1);
-            
+            Cv2.Circle(mask_img, (OpenCvSharp.Point)center, (int)(radius + 7), 255, thickness: -1);
+
             //outer contour2 in order to make mask area = 255
             Mat mask_img2 = new Mat(Src.Size(), MatType.CV_8UC1, new Scalar(255));//initilize Mat with the value 255
-            Cv2.Circle(mask_img2, (OpenCvSharp.Point)center, (int)(radius +7), 0, thickness: -1);
+            Cv2.Circle(mask_img2, (OpenCvSharp.Point)center, (int)(radius + 7), 0, thickness: -1);
 
 
             Mat image = Mat.Zeros(Src.Size(), MatType.CV_8UC1);
@@ -97,28 +98,59 @@ namespace Stop3
             image = image + mask_img2;
             //image.SaveImage("./mask.jpg");
             //============================now already find the ROI, start algorithm=====================================
-            Cv2.Blur(image,image, ksize: new Size(blur_size,blur_size));
+            Cv2.Blur(image, image, ksize: new Size(blur_size, blur_size));
             //image.SaveImage("./mask2.jpg");
-            
+
             //==========================create 0.5 degree a line===================================================
             double factor = 3.141592653589793 / 180;
-            List<int> x = new List<int>();
-            List<int> y = new List<int>();
-            List<int> inner_x = new List<int>();
-            List<int> inner_y = new List<int>();
+            List<Point> outer_index = new List<Point>();
+            List<Point> inner_index = new List<Point>();
             double degree_delta = 0.5;
-            int cx = (int) center.X ;
+            int cx = (int)center.X;
             int cy = (int)center.Y;
             int r = (int)radius;
-            Console.WriteLine(r + " "+ cx + " "+ cy);
-            for(int degree = 0; degree < (360 / degree_delta); degree++)
+            //Console.WriteLine(r + " "+ cx + " "+ cy);
+
+            for (int degree = 0; degree < (360 / degree_delta); degree++)
             {
                 double degree_real = degree * degree_delta;
-                //int now_x = ; 
+                int now_x = (int)((r + 2) * Math.Sin(degree_real * factor)) + cx;
+                int now_y = (int)((r + 2) * Math.Cos(degree_real * factor)) + cy;
+                int now_inner_x = (int)((r - 30) * Math.Sin(degree_real * factor)) + cx;
+                int now_inner_y = (int)((r - 30) * Math.Cos(degree_real * factor)) + cy;
+
+                inner_index.Add(new Point(now_inner_x,now_inner_y));
+                outer_index.Add(new Point(now_x, now_y));
             }
+            //==========================shot from center=========================================
+            for (int degree = 0; degree < (360 / degree_delta); degree++)
+            {
+                double a = 0;
+                //var A = NumSharp.np.linspace(new Point(), 200, ref a, 200, dtype: NumpyDotNet.np.Int64);
+                /*var A = NumSharp.np.linspace(new Point(0,0), new Point(200,200), ref a, 200, dtype: NumSharp.np.int64);
+                if (degree == 0)
+                {
+                    var points_on_line_x = ((IEnumerable<double>)Enumerable.Range(inner_x[degree], x[degree]).Select(i => (double)i * 0.01)).ToArray();
+                    var points_on_line_y = ((IEnumerable<double>)Enumerable.Range(inner_y[degree], y[degree]).Select(i => (double)i * 0.01)).ToArray();
+                    Console.WriteLine(points_on_line_x.Length);
+                    Console.WriteLine(points_on_line_y.Length);
+                }*/
+                
+                   using(System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\Chernger\\Desktop\\plot_data\\raw_data\\" + degree + ".txt", false)) {  
+                foreach (var lip in new LineIterator(image, inner_index[degree], outer_index[degree]))
+                {
+                        
+                        
+                            file.WriteLine(lip.GetValue<byte>());
+                        
+                        //Console.WriteLine(lip.GetValue<byte>());
+                }
+                    }
+                
 
 
-
+            }
         }
+
     }
 }
